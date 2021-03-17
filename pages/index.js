798101632e81302501f0e5e2cd3e08f5aa243d59
gacarrasquilla <gennys.carrasquilla@platypus-agency.com>
2021-03-17 00:15:29 -0500

    edit product

--- pages/index.js
+++ pages/index.js
@@ -9,6 +9,7 @@ const img = "https://cdn.shopify.com/s/files/1/0757/9955/files/empty-state.svg"
 
 const Index = () => {
   const [open, setOpen] = useState(false)
+  const emptyState = !store.get("ids")
 
   const handleSelection = (resources) => {
     const idsFromResources = resources.selection.map((product) => product.id)
@@ -33,19 +34,22 @@ const Index = () => {
         onSelection={(resources) => handleSelection(resources)}
         onCancel={() => setOpen(false)}
       />
-      <Layout>
-        <EmptyState
-          heading="Discount your products temporarily"
-          action={{
-            content: "Select products -a",
-            onAction: () => setOpen(true),
-          }}
-          image={img}
-        >
-          <p>Select products to change their price temporarily.</p>
-        </EmptyState>
-      </Layout>
-      <ResourceListWithProducts />
+      {emptyState ? (
+        <Layout>
+          <EmptyState
+            heading="Discount your products temporarily"
+            action={{
+              content: "Select products -a",
+              onAction: () => setOpen(true),
+            }}
+            image={img}
+          >
+            <p>Select products to change their price temporarily.</p>
+          </EmptyState>
+        </Layout>
+      ) : (
+        <ResourceListWithProducts />
+      )}
     </Page>
   )
 }
